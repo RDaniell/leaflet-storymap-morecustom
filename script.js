ac90c19b713605e4c4a2181593f4986c7735dc91
racheldaniell <44450960+racheldaniell@users.noreply.github.com>
2021-04-16 09:25:03 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,96 @@
+var imageContainerMargin = 70;  // Margin + padding
+
+// This watches for the scrollable container
+var scrollPosition = 0;
+$('div#contents').scroll(function() {
+  scrollPosition = $(this).scrollTop();
+});
+
+function initMap() {
+
+  // This creates the Leaflet map with a generic start point, because code at bottom automatically fits bounds to all markers
+  var map = L.map('map', {
+    center: [0, 0],
+    zoom: 5,
+    scrollWheelZoom: false
+  });
+
+  // This displays a base layer map (other options available)
+  var lightAll = new L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  ,
+  subdomains: 'abcd',
+  minZoom: 1,
+  maxZoom: 20,
+  ext: 'jpg'}).addTo(map);
+
+  // This customizes link to view source code; add your own GitHub repository
+  map.attributionControl
+  .setPrefix('View <a href="http://github.com/racheldaniell/leaflet-storymap-basic" target="_blank">code on GitHub</a>, created with <a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a>');
+
+  // This loads the GeoJSON map data file from a local folder
+  $.getJSON('map.geojson', function(data) {
+    var geojson = L.geoJson(data, {
+      onEachFeature: function (feature, layer) {
+        (function(layer, properties) {
+          // This creates numerical icons to match the ID numbers
+          // OR remove the next 6 lines for default blue Leaflet markers
+          var numericMarker = L.ExtraMarkers.icon({
+            icon: 'penta',
+            number: feature.properties['id'],
+            markerColor: 'black'
+          });
+          layer.setIcon(numericMarker);
+
+          // This creates the contents of each chapter from the GeoJSON data. Unwanted items can be removed, and new ones can be added
+          var containerSource = $("#container-template").html();
+          var containerTemplate = Handlebars.compile(containerSource);
+
+          var output = {
+            "containerId": 'container' + feature.properties['id'],
+            "chapter": feature.properties['chapter'],
+            "imgSrc": feature.properties['image'],
+            "srcHref": feature.properties['source-link'],
+            "srcText": feature.properties['source-credit'],
+            "description": feature.properties['description']
+          }
+          var html = containerTemplate(output);
+          $('#contents').append(html);
+
+          var i;
+          var areaTop = -100;
+          var areaBottom = 0;
+
+          // Calculating total height of blocks above active
+          for (i = 1; i < feature.properties['id']; i++) {
+            areaTop += $('div#container' + i).height() + imageContainerMargin;
+          }
+
+          areaBottom = areaTop + $('div#container' + feature.properties['id']).height();
+
+          $('div#contents').scroll(function() {
+            if ($(this).scrollTop() >= areaTop && $(this).scrollTop() < areaBottom) {
+              $('.image-container').removeClass("inFocus").addClass("outFocus");
+              $('div#container' + feature.properties['id']).addClass("inFocus").removeClass("outFocus");
+
+              map.flyTo([feature.geometry.coordinates[1], feature.geometry.coordinates[0] ], feature.properties['zoom']);
+            }
+          });
+
+          // Make markers clickable
+          layer.on('click', function() {
+            $("div#contents").animate({scrollTop: areaTop + "px"});
+          });
+
+        })(layer, feature.properties);
+      }
+    });
+
+    $('div#container1').addClass("inFocus");
+    $('#contents').append("<div class='space-at-the-bottom'><a href='#space-at-the-top'><i class='fa fa-chevron-up'></i></br><small>Top</small></a></div>");
+    map.fitBounds(geojson.getBounds());
+    geojson.addTo(map);
+  });
+}
+
+initMap();
